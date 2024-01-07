fc77840547ce371d6857bd30f823f23c638f4cf3
skl.progs <skl.progs@gmail.com>
2024-01-07 22:48:44 +0300

    Set lists

---
+++ src/logic.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import skl_shared.shared as sh
+from skl_shared.localize import _
+
+
+class Replacer:
+    
+    def __init__(self, file, what, with_):
+        self.Success = True
+        self.file = file
+        self.what = what
+        self.with = with_
+        self.text = ''
+    
+    def check(self):
+        f = '[MultiReplacer] logic.Replacer.check'
+        if not self.file or not self.what or not self.with:
+            self.Success = False
+            sh.com.rep_empty(f)
+            return
+        self.Success = sh.File(self.file).Success
+        if len(self.what) != len(self.with):
+            self.Success = False
+            sub = f'{len(self.what)} == {len(self.with)}'
+            mes = _('The condition {} is not observed!').format(sub)
+            sh.objs.get_mes(f, mes, True).show_warning()
+    
+    def set_text(self):
+        f = '[MultiReplacer] logic.Replacer.set_text'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.text = sh.ReadTextFile(self.file, Empty=True)
+    
+    def replace(self):
+        f = '[MultiReplacer] logic.Replacer.replace'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        for i in range(len(self.what)):
+            self.text = self.text.replace(self.what[i], self.with[i])
+    
+    def save(self):
+        f = '[MultiReplacer] logic.Replacer.save'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        self.Success = sh.WriteTextFile(self.file, True, True).write(self.text)
+    
+    def run(self):
+        self.check()
+        self.set_text()
+        self.replace()
+        self.save()
--- src/multi_replacer.py
+++ src/multi_replacer.py
@@ -44,11 +44,77 @@ class Walker:
         self.filter()
 
 
+
+class Replacer:
+    
+    def __init__(self):
+        self.set_values()
+    
+    def set_values(self):
+        self.Success = True
+        self.text = ''
+        self.what = []
+        self.with_ = []
+    
+    def get_input(self):
+        f = '[MultiReplacer] multi_replacer.Replacer.get_input'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        text = sh.Clipboard().paste()
+        #text = sh.com.run_fast_txt(text)
+        title = _('Enter tab-delimited text:')
+        sh.objs.get_txt().reset(title=title, text=text)
+        sh.objs.txt.show()
+        self.text = sh.objs.txt.get()
+    
+    def set_lists(self):
+        f = '[MultiReplacer] multi_replacer.Replacer.set_lists'
+        if not self.Success:
+            sh.com.cancel(f)
+            return
+        if not self.text:
+            self.Success = False
+            sh.com.rep_empty(f)
+            return
+        lists = self.text.splitlines()
+        lists = [line.strip() for line in lists if line.strip()]
+        if not lists:
+            self.Success = False
+            sh.com.rep_empty(f)
+            return
+        if len(lists) != self.text.count('\t'):
+            self.Success = False
+            sub = '{} == {}'.format(len(lists), self.text.count('\t'))
+            mes = _('The condition "{}" is not observed!').format(sub)
+            sh.objs.get_mes(f, mes, True).show_warning()
+            return
+        for i in range(len(lists)):
+            if lists[i].count('\t') != 1:
+                self.Success = False
+                mes = _('Wrong input data "{}" at row #{}!').format(lists[i], i)
+                sh.objs.get_mes(f, mes, True).show_warning()
+                return
+            what, with_ = lists[i].split('\t')
+            self.what.append(what)
+            self.with_.append(with_)
+    
+    def run(self):
+        self.get_input()
+        self.set_lists()
+
+
 if __name__ == '__main__':
     sh.com.start()
+    '''
     path = '/home/pete/bin/mclient'
     iwalker = Walker(path)
     iwalker.run()
     text = '\n'.join(iwalker.files)
     print(text)
+    '''
+    irepl = Replacer()
+    irepl.run()
+    mes = 'What:\n{}\n\nWith:\n{}'.format("\n".join(irepl.what), "\n".join(irepl.with_))
+    print(mes)
     sh.com.end()
